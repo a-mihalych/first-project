36fa67ecfa909249207a30870cc185ee448977fc
a-mihalych <a-mihalych@yandex.ru>
2022-07-24 15:35:30 +0400

    feat: вовод курса

--- Praktikum.java
+++ Praktikum.java
@@ -1,8 +1,12 @@
 public class Praktikum {
 	public static void main(String[] args) {
 		int j = 9;
-		for (int i = 0; i < j; i++) {
-			System.out.println("i = " + i);
+		Converter converter = new Converter();
+		double процент;
+		for (int i = 1; i < j; i++) {
+			процент = 1 + ((Math.random() - 0.5) / 100);
+			converter.руб *= процент;
+			System.out.printf("Курс %d = %.2f\n", i, converter.руб);
 		}
 	}
 }
